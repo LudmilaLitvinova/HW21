b775fe9028b7b63f4ba04603e72c8627d477a548
LudmilaLitvinova <ludik-1984@ukr.net>
2023-01-08 17:09:22 +0200

    Fix issues from review

--- src/main/java/com/ua/hillellit/model/Calculation.java
+++ src/main/java/com/ua/hillellit/model/Calculation.java
@@ -1,6 +1,6 @@
 package com.ua.hillellit.model;
 
-import java.io.File;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -11,21 +11,26 @@ public class Calculation {
     public Map<String, List<String>> calculateResultByStore(List<Order> list) {
 
         Map<String, List<String>> resultByStore = new HashMap<>();
-        Set<String> storeNames = list.stream().map(el -> el.getStoreName()).collect(Collectors.toSet());
+        Set<String> storeNames = list.stream().map(Order::getStoreName).collect(Collectors.toSet());
 
         for (String name : storeNames) {
 
             List<String> productsList = new ArrayList<>();
 
-            List<Order> curList = list.stream().filter(o -> o.getStoreName().equals(name)).collect(Collectors.toList());
-
+            List<Order> curList = list.stream().filter(o ->
+                    o.getStoreName().equals(name)).collect(Collectors.toList());
 
             Map<String, Integer> countsByProduct = curList.stream().collect(
-                    Collectors.groupingBy(Order::getProductName, Collectors.summingInt(Order::getCountOfProduct)));
+                    Collectors.groupingBy(
+                            Order::getProductName,
+                            Collectors.summingInt(Order::getCountOfProduct)));
             Map<String, Double> avgPriceByProduct = curList.stream().collect(
-                    Collectors.groupingBy(Order::getProductName, Collectors.averagingDouble(Order::getProductPrice)));
+                    Collectors.groupingBy(
+                            Order::getProductName,
+                            Collectors.averagingDouble(Order::getProductPrice)));
             for (Map.Entry<String, Double> item : avgPriceByProduct.entrySet()) {
-                String text = item.getKey() + ";" + String.format("%.2f", item.getValue()) + ";" + countsByProduct.get(item.getKey()) + ";";
+                String text = item.getKey() + ";" + String.format("%.2f", item.getValue()) + ";"
+                        + countsByProduct.get(item.getKey()) + ";";
                 productsList.add(text);
             }
             resultByStore.put(name, productsList);
